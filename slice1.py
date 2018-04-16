5ce8bcc62b753b78209023a1c3f861cad231239d
Hikaru Nakamura <hikaru.0601.wed@gmail.com>
2018-04-16 11:07:34 +0900

    add slice
    add slice

---
+++ slice1.py
@@ -0,0 +1,10 @@
+verb='singing'
+
+print(verb[:])
+
+print(verb[:4])
+print(verb[4:])
+
+print(verb[-3:])
+
+print(verb[2:4])
---
+++ slice2.py
@@ -0,0 +1,10 @@
+str='1,2,3,4,5,6,7,8,9'
+
+# string[start index:end index:step]
+print(str[::1])
+print(str[::2])
+
+print(str[2:-2:1])
+print(str[2:-2:2])
+
+print(str[::-1])
