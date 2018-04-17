75b4ad6690e483eedb0341fdc44a062372a3eaf4
Hikaru Nakamura <hikaru.0601.wed@gmail.com>
2018-04-17 10:36:09 +0900

    add format
    add format

---
+++ str_format1.py
@@ -0,0 +1,13 @@
+top='現在'
+day='10日'
+am='午前'
+pm='午後'
+
+sentence='{first}、{second}の{third}です。'.format(
+    first=top,
+    second=day,
+    third=am)
+
+print(sentence)
+
+
---
+++ str_format2.py
@@ -0,0 +1,3 @@
+cm=float(input('cmを入力して下さい。--->'))
+
+print('{:.3f}cmは{:.3f}インチです。'.format(cm,cm/2.54))
