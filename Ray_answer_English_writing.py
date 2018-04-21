be486b723f1b2d2dc02d0b608d661bb394d2cef3
Hikaru Nakamura <hikaru.0601.wed@gmail.com>
2018-04-21 15:09:31 +0900

    add Ray_answer_English_writing
    add Ray_answer_English_writing

---
+++ Ray_answer_English_writing.py
@@ -0,0 +1,35 @@
+# 第1文型
+print('第1文型の例文を作るね')
+subject=input('主語(S)は？--->')
+verb=input('動詞(V)は？--->')
+print('{s} {v}.'.format(s=subject,v=verb))
+
+# 第2文型
+print('第2文型の例文を作るね')
+subject=input('主語(S)は？--->')
+verb=input('動詞(V)は？--->')
+comp=input('補語(C)は？--->')
+print('{s} {v} {c}.'.format(s=subject,v=verb,c=comp))
+
+# 第3文型
+print('第3文型の例文を作るね')
+subject=input('主語(S)は？--->')
+verb=input('動詞(V)は？--->')
+comp=input('補語(C)は？--->')
+print('{s} {v} {c}.'.format(s=subject,v=verb,c=comp))
+
+# 第4文型
+print('第4文型の例文を作るね')
+subject=input('主語(S)は？--->')
+verb=input('動詞(V)は？--->')
+i_obj=input('間接目的語(IO)は？--->')
+d_obj=input('間接目的語(DO)は？--->')
+print('{s} {v} {io} {do}.'.format(s=subject,v=verb,io=i_obj,do=d_obj))
+
+# 第5文型
+print('第5文型の例文を作るね')
+subject=input('主語(S)は？--->')
+verb=input('動詞(V)は？--->')
+obj=input('目的語(O)は？--->')
+comp=input('補語(C)は？--->')
+print('{s} {v} {o} {c}.'.format(s=subject,v=verb,o=obj,c=comp))
